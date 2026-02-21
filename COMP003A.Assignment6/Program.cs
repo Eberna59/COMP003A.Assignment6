ff7e43017d5674aa4240555710929a759d306070
evanbern59 <evansantos1202@gmail.com>
2026-02-21 13:15:59 -0800

    added main program loop for user choices and then added display menu

--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -7,6 +7,18 @@
             Book book1 = new Book("The Green Mile", 592, true);
             Book book2 = new Book("The Martian", 369, true);
             Book book3 = new Book("The Long Walk", 370, false);
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\n1. Display All Books");
+                Console.WriteLine("2. Update One Book");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter Choice: ");
+
+                string choice = Console.ReadLine();
+            }
         }
     }
 }
