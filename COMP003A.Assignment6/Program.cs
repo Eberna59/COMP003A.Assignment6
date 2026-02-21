778845e5bfa02dfa7a39056acdd8a31e6b42b4a7
evanbern59 <evansantos1202@gmail.com>
2026-02-21 13:08:14 -0800

    model is a libaray of what books ive read and want to read

--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Co
+            Book book1 = new Book("The Green Mile", 592, true);
+            Book book2 = new Book("The Martian", 369, true);
+            Book book3 = new Book("The Long Walk", 370, false);
         }
     }
 }
