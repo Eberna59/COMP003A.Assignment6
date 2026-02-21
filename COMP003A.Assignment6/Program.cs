96fbe250cdedbf65dd8ce80c61950a7752095c43
evanbern59 <evansantos1202@gmail.com>
2026-02-21 13:42:00 -0800

    finished project

--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -3,15 +3,15 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Book book1 = new Book("The Green Mile", 592, true);
+        {    // Book Tracker
+            Book book1 = new Book("The Green Mile", 592, true); // 3 sepaart books to display for updates and complete status.
             Book book2 = new Book("The Martian", 369, true);
             Book book3 = new Book("The Long Walk", 370, false);
 
-            bool running = true;
+            bool running = true; // controls when the menu continyes running
 
-            while (running)
-            {
+            while (running) 
+            {   // user meny options
                 Console.WriteLine("\n1. Display All Books");
                 Console.WriteLine("2. Update One Book");
                 Console.WriteLine("3. Exit");
@@ -19,7 +19,7 @@
 
                 string choice = Console.ReadLine();
 
-                switch (choice)
+                switch (choice) // determines which menu option the user picks
                 {
                     case "1":
                         Console.WriteLine("\nBook 1:");
@@ -54,6 +54,10 @@
                                 break;
                         }break;
 
+                    case "3": // ends the loops and stops program
+                        running = false;
+                        Console.WriteLine("Progran ended");
+                        break;
 
                 }
             }
