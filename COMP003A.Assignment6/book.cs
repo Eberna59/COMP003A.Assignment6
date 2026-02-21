528aabbb8d844ddc2a39d0b237a38632f5da5411
evanbern59 <evansantos1202@gmail.com>
2026-02-21 10:58:31 -0800

    models a libaray book with a title, page numbers, and complete status.

---
+++ COMP003A.Assignment6/book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.Assignment6
+{
+    internal class book
+    {
+        public string Title;
+
+        public int Pages;
+
+        public bool Completed;
+
+
+    }
+}
